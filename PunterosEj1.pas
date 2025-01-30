d722bd382288e49d2d5f0e1c6f61fdd144955a62
Fran-G-G <f.garcia.2024@alumnos.urjc.es>
2025-01-30 12:07:31 +0100

    Update PunterosEj1.pas

--- PunterosEj1.pas
+++ PunterosEj1.pas
@@ -8,28 +8,37 @@ uses
     ctr;
 
 var
-    { Declara variable entera (x) }
-    { Declara una variable de tipo puntero a entero (p_ent) }
+    x: integer; { Declara variable entera (x) }
+    p_ent: ^integer; { Declara una variable de tipo puntero a entero (p_ent) }
 
 begin
 
-    { Da el valor 100 a x }
+    x:= 100; { Da el valor 100 a x }
 
     { Crea un entero dinámicamente con p_ent y dale el valor que tiene actualmente x }
+    New(p_ent);
+    p_ent^:= x;
 
     { Imprime por pantalla el valor contenido en el entero al que apunta p_ent }
+    writeln('Valor de p_ent^: ', p_ent^);
 
     { Crea con new un nuevo entero dinámicamente }
+    New(p_ent);
 
     { Imprime por pantalla el valor contenido en el entero al que apunta p_ent }
+    writeln('Valor de p_ent^ después del New: ', p_ent^);
 
     { Pon el puntero p_ent a apuntar a x }
+    p_ent := @x;
 
     { Súmale 100 al entero apuntado por el puntero p_ent }
+    p_ent^ := p_ent^ + 100;
 
     { Imprime por pantalla el valor de x y también del entero al que apunta p_ent }
+    writeln('Valor de x después de sumar 100: ', x, '. Valor de p_ent^ volviendo a apuntar a x: ', p_ent^);
 
     { Pon el puntero p_ent a NIL }
+    p_ent := nil;
 
     { Libera la memoria asociada al nuevo entero }
 
